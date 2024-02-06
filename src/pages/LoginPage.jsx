8dfb170a64286d7b8f1f80a0ae05a10f5c1628df
Igor <qwerty2244668800@gmail.com>
2024-02-06 22:28:42 +0200

    google authorization is implemented

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,45 @@
 import React from "react";
 import RegistrationForm from "../features/login-registration/RegistrationForm";
+import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+    const navigate = useNavigate();
+    const googleLoginHandler = async (e) => {
+        try {
+            const auth = getAuth();
+            const provider = new GoogleAuthProvider();
+            const result = await signInWithPopup(auth, provider);
+            const user = result.user;
+
+            // check for the user
+
+            const docRef = doc(db, "users", user.uid);
+            const docSnap = await getDoc(docRef);
+            if (!docSnap.exists()) {
+                await setDoc(docRef, {
+                    name: user.displayName,
+                    email: user.email,
+                    timestamp: serverTimestamp(),
+                });
+            }
+            navigate("/");
+        } catch (err) {
+            console.log(err);
+            toast.error("Could not authorize with google");
+        }
+    };
     return (
         <div>
             <RegistrationForm />
+            <button type="button" onClick={googleLoginHandler}>
+                <FcGoogle />
+                Continue with Google
+            </button>
         </div>
     );
 };
