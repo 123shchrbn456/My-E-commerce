44843156c6a440047e435033e1e63342628ec643
Igor <qwerty2244668800@gmail.com>
2024-02-10 16:46:21 +0200

    edit profile functionality with firebase is implemented

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,6 +1,9 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
 
 const ProfilePage = () => {
     const auth = getAuth();
@@ -9,10 +12,39 @@ const ProfilePage = () => {
         name: auth.currentUser.displayName,
         email: auth.currentUser.email,
     });
+    const [editMode, setEditMode] = useState(false);
 
     const logoutHandler = () => {
         auth.signOut();
         navigate("/");
+    };
+
+    const changeNameHandler = (e) => {
+        setFormData((prevData) => ({
+            ...prevData,
+            name: e.target.value,
+        }));
+    };
+
+    const submitHandler = async () => {
+        try {
+            if (auth.currentUser.displayName !== formData.name) {
+                // update display name in firebase auth
+                await updateProfile(auth.currentUser, {
+                    displayName: formData.name,
+                });
+
+                // update name in the firestore
+                const docRef = doc(db, "users", auth.currentUser.uid);
+                await updateDoc(docRef, {
+                    name: formData.name,
+                });
+                toast.success("Profile details are updated");
+            }
+        } catch (err) {
+            console.error(err);
+            toast.error("Could not update the profile");
+        }
     };
 
     return (
@@ -20,14 +52,21 @@ const ProfilePage = () => {
             <h1>My profile</h1>
             <form>
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" value={formData.name} disabled />
+                <input type="text" id="name" value={formData.name} disabled={!editMode} onChange={changeNameHandler} />
                 <label htmlFor="email">Email</label>
                 <input type="text" id="email" value={formData.email} disabled />
             </form>
             <div>
                 <p>
                     Do you want to change your name?
-                    <button>Edit</button>
+                    <button
+                        onClick={() => {
+                            if (editMode) submitHandler();
+                            setEditMode((prevValue) => !prevValue);
+                        }}
+                    >
+                        {editMode ? "Apply change" : "Edit"}
+                    </button>
                 </p>
                 <button onClick={logoutHandler}>Sign out</button>
             </div>
